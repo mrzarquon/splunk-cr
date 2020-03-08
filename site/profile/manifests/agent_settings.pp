8f0b92263c1bcd4af2655919cee6e5fbf15facfd
Chris Barker <cbarker@puppet.com>
2020-03-08 16:53:51 -0700

    add agent settings

---
+++ site/profile/manifests/agent_settings.pp
@@ -0,0 +1,26 @@
+# set us up for runs every 5 minutes so we have some traffic
+
+class profile::agent_settings {
+
+  Pe_ini_setting {
+    ensure  => present,
+    path    => "${::puppet_enterprise::params::confdir}/puppet.conf",
+    section => 'agent',
+  }
+
+  pe_ini_setting { 'enable splay':
+    setting => 'splay',
+    value   => true,
+  }
+
+  pe_ini_setting { 'set splaylimit':
+    setting => 'splaylimit',
+    value   => '5m',
+  }
+
+  pe_ini_setting { 'shorten runinterval':
+    setting => 'runinterval',
+    value   => '5m',
+  }
+
+}
