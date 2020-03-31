00864ab1b7a725817eee21e60bc79f1ba319246f
Chris Barker <cbarker@puppet.com>
2020-03-30 19:15:47 -0700

    rename patching to remediate

--- site/profile/manifests/remediate.pp
+++ site/profile/manifests/remediate.pp
@@ -2,20 +2,17 @@
 
 class profile::remediate {
 
+  Package <| title == 'chrony' |> {
+    tag    => ['pkg_locked'],
+  }
+
   $pkg_patches = lookup('pkg_patches')
 
   $pkg_patches.each | $pkgname, $pkgversion | {
 
-    Package <| title == $pkgname |> {
+    Package <| title == $pkgname and tag != 'pkg_locked' |> {
       ensure => $pkgversion,
       tag    => ['remediate_patched','remediate_overriden'],
     }
   }
-
-  # Package <| title == 'chrony' |> {
-  #   ensure => '3.5-1.el8',
-  #   tag    => ['patched'],
-  # }
-
-
 }
