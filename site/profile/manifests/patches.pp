43a270b3a89b939c9f1a9d6634c54b1cfcf104ac
Chris Barker <cbarker@puppet.com>
2020-03-30 18:45:22 -0700

    add patching example

---
+++ site/profile/manifests/patches.pp
@@ -0,0 +1,18 @@
+# a really dumb class to prove a way to do patches
+
+class profile::patches {
+
+  # $patches = lookup('patches')
+
+  # patches.each | $pkgname, $pkgversion | {
+  #   Package <| title == $pkgname |> {
+  #     ensure => $pkgversion,
+  #   }
+  # }
+
+  Package <| title == 'chrony' |> {
+    ensure => '3.5-1.el8'
+  }
+
+
+}
